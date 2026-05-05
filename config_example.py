2af5fe1059c91f12b72a9ee81a6d93c7389d3a63
saradhvi <saradhvisrinivasan22@gmail.com>
2026-05-05 12:47:14 +0530

    Removed config.py and added config_example.py

---
+++ config_example.py
@@ -0,0 +1,11 @@
+SECRET_KEY = "your_secret_key_here"
+DATABASE = "database.db"
+
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = "your_email@gmail.com"
+MAIL_PASSWORD = "your_app_password"
+
+RAZORPAY_KEY_ID = "rzp_test_xxxxx"
+RAZORPAY_KEY_SECRET = "your_secret_key"
